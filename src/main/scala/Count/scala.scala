5eec259c5a8bcf4fa22544d023f0ff4c805a4d3d
beketx <beketsk@gmail.com>
2020-09-14 20:40:54 +0600

    Not finished

---
+++ src/main/scala/Count/scala.scala
@@ -0,0 +1,21 @@
+package Count
+package parsers
+
+import scala.util.parsing.combinator._
+
+class scala {
+  def expr : Parser[Double] = term ~ rep (("+" | "-") ~ term)  ^^ eval
+
+  def term : Parser[Double] = factor ~ rep (( "*" | "/") ~ term) ^^ eval
+
+  def factor : Parser[Double] = ( floatingPointNumber ^^ (_.toDouble) | "(" ~> expr <~")"))
+
+  def eval:Double ~ List[String ~ Double] => Double - {
+    case v ~ list => (v /: list) {
+      case (op1, "+" ~ op2) => op1 + op2
+      case (op1, "-" ~ op2) => op1 - op2
+      case (op1, "*" ~ op2) => op1 * op2
+      case (op1, "/" ~ op2) => op1 / op2
+  }
+  }
+}
