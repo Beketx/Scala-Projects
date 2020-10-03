97a3db670bb1d9e95a21a00f2cc72beca65c83b0
beketx <beketsk@gmail.com>
2020-10-03 15:26:31 +0600

    123

--- week5Quiz/src/main/scala/MinimumDistanceBetweenBSTNodes.scala
+++ week5Quiz/src/main/scala/MinimumDistanceBetweenBSTNodes.scala
@@ -1,5 +1,21 @@
+object MinimumDistanceBetweenBSTNodes{
+  def minDiffInBST(root: TreeNode): Int = {
+    var arr = Array[Int]()
+
+    def main(node: TreeNode = root): Unit = {
+      if (node != null) {
+        main(node.left)
+        arr :+= node.value
+        main(node.right)
+      }
+    }
+
+    main()
+    (0 until arr.length - 1).map(i => math.abs(arr(i + 1) - arr(i))).min
+  }
 
 
+}
 
 
 
